2ed0d25ea44c355dd856f56c795be646f855dcbb
emadpoursina <emad.puursina@gmail.com>
2021-10-31 15:09:22 +0330

    update wget-command-generator

--- wget-command-generator.js
+++ wget-command-generator.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 "use strict";
 
-const getStdin = require('get-stdin');
 
 /**
  * It will generate wget commands for
@@ -9,20 +8,46 @@ const getStdin = require('get-stdin');
  * e.g: /*S{0-9}{0-9}E{0-9}{0-9}\*\/
  */
 
-const startEpisode = 1;
-const endEpisode = 13;
-const fileName = "naruto";
-const url = "";
 
+const args = require('minimist')(process.argv.slice(2), {
+	string: ["outputName", "url"],
+	boolean: ["help"]	
+});
 
-let output = "";
-
-for (let i = startEpisode; i <= endEpisode; i++) {
-	if(i < 10) {
-		output += `wget -c -O ${fileName}-0${i} https://s3.my-film.pw/serial/Naruto/05-480p.x264.Dubbed.FA/Naruto.S05E0${i}.HDTV.480p.x264.Dubbed.FA.mkv;`;
-	}else {
-		output += `wget -c -O ${fileName}-${i} https://s3.my-film.pw/serial/Naruto/05-480p.x264.Dubbed.FA/Naruto.S05E${i}.HDTV.480p.x264.Dubbed.FA.mkv;`;	
-	}
+if(args.help) {
+	printHelp();	
+}else {
+	console.log('Bad arguments');
+	console.log('');
+	printHelp();
 }
 
-console.log(output);
+const startEpisode = 1;
+const endEpisode = 13;
+let output = "";
+
+//***********************************/
+function commandGenerator(startEpisode, endEpisode, outputName) {
+	let output = "";
+
+	// Generate the download command for each episode
+	for (let i = startEpisode; i <= endEpisode; i++) {
+		if(i < 10) {
+			output += `wget -c -O ${outputName}-0${i} https://s3.my-film.pw/serial/Naruto/05-480p.x264.Dubbed.FA/Naruto.S05E0${i}.HDTV.480p.x264.Dubbed.FA.mkv;`;
+		}else {
+			output += `wget -c -O ${outputName}-${i} https://s3.my-film.pw/serial/Naruto/05-480p.x264.Dubbed.FA/Naruto.S05E${i}.HDTV.480p.x264.Dubbed.FA.mkv;`;	
+		}
+	}
+
+	return output;
+}
+
+function printHelp() {
+	console.log('Usage:');
+	console.log('wget-command-generator --url={FILEURL} --startEpisode={first_episode} --endEpisode={end_episdoe}');
+	console.log('');
+	console.log('--help						Getting help');
+	console.log('--output={FILENAME}				For setting end file name');
+	console.log('						Default: outputSssEee.mp4');
+	console.log('');
+}
